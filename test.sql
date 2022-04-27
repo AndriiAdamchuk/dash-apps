cbed96ab7d1f6ea071c2220c1af1317c7205240d
Andrii Adamchuk <andrii.adamchuk@gmail.com>
2022-04-27 12:46:48 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -3,7 +3,8 @@ select
     client_name,
     client_city,
     client_country,
-    sum(revenue_total) as revenue_total
+    sum(revenue_total) as revenue_total,
+    date_trunc('day', period) period
 from reports.clients
-group by 1, 2, 3
+group by 1, 2, 3, 4
 order by 1 asc;
